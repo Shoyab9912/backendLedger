5c798d4013cec377672ef9f931fc7501cfc2d410
deadly9912 <mashoyab703@gmail.com>
2026-03-04 10:17:09 +0530

    add transaction routes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import { errorHandler } from "./middlewares/errorHandler.middeleware.js";
 import authRoutes from "./routes/auth.route.js";
 import accountRoutes from "./routes/account.route.js";
+import transactionRoutes from "./routes/transaction.route.js"
+
 const app = express();
 
 app.use(express.json({ limit: "2mb" }));
@@ -11,6 +13,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/accounts", accountRoutes);
+app.use("/api/transactions",transactionRoutes);
+
 
 app.use(errorHandler);
 export { app };
---
+++ src/routes/transaction.route.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { createTransaction } from "../controllers/transaction.controller.js";
+
+const router = express.Router()
+
+
+router.post('/',createTransaction)
+
+export default router;
